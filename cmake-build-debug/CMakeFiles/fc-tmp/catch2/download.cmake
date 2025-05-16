e73a58a70c11560342ff84b340ac6c9492a9c4af
Daniele <34173730+kr0bi@users.noreply.github.com>
2025-05-16 11:42:19 +0200

    Delete cmake-build-debug directory

--- cmake-build-debug/CMakeFiles/fc-tmp/catch2/download.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing download step for catch2")
-
-block(SCOPE_FOR VARIABLES)
-
-include("/home/daniele/CLionProjects/satp/cmake-build-debug/CMakeFiles/fc-tmp/catch2/catch2-gitclone.cmake")
-
-endblock()
--- cmake-build-debug/CMakeFiles/fc-tmp/catch2/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing update step for catch2")
-
-block(SCOPE_FOR VARIABLES)
-
-include("/home/daniele/CLionProjects/satp/cmake-build-debug/CMakeFiles/fc-tmp/catch2/catch2-gitupdate.cmake")
-
-endblock()
