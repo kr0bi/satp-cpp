c278ce6f67e33687c9d2e7759a585588646073fd
daniele.ferroli <daniele+dev@ferroli.ovh>
2026-01-25 14:49:37 +0100

    Correzione della formula RSE e aggiunta del test di HLL e HLL++

--- tests/algorithms/HyperLogLog.cpp
+++ tests/algorithms/HyperLogLog.cpp
@@ -11,17 +11,39 @@ TEST_CASE("HyperLogLog stima ~1000 distinti su 10000 campioni", "[hyperloglog-co
     auto dataset = satp::testdata::loadDataset();
     auto NUMBER_OF_UNIQUE_ELEMENTS = dataset.distinct;
 
-    satp::algorithms::HyperLogLogPlusPlus loglog(K);
-    satp::simulation::Loop loop(std::move(loglog), std::move(dataset.values));
+    satp::algorithms::HyperLogLog hll(K, 32);
+    satp::simulation::Loop loop(std::move(hll), std::move(dataset.values));
 
     auto estimate = loop.process();
 
     WARN("Stima = " << estimate);
     WARN("Elementi = " << NUMBER_OF_UNIQUE_ELEMENTS);
 
-    auto denom = 1.0 / std::sqrt(double(1u << K));
-    const double RSE = 1.0 / denom;
-    WARN("RSE = " << 1.0 / denom);
+    const double m = double(1u << K);
+    const double RSE = 1.04 / std::sqrt(m);
+    WARN("RSE = " << RSE);
+
+    REQUIRE(estimate >= NUMBER_OF_UNIQUE_ELEMENTS * (1.0 - 3 * RSE));
+    REQUIRE(estimate <= NUMBER_OF_UNIQUE_ELEMENTS * (1.0 + 3 * RSE));
+}
+
+TEST_CASE("HyperLogLog++ stima ~1000 distinti su 10000 campioni", "[hyperloglogpp-count]") {
+    constexpr std::uint32_t K = 10;
+
+    auto dataset = satp::testdata::loadDataset();
+    auto NUMBER_OF_UNIQUE_ELEMENTS = dataset.distinct;
+
+    satp::algorithms::HyperLogLogPlusPlus hllpp(K);
+    satp::simulation::Loop loop(std::move(hllpp), std::move(dataset.values));
+
+    auto estimate = loop.process();
+
+    WARN("Stima = " << estimate);
+    WARN("Elementi = " << NUMBER_OF_UNIQUE_ELEMENTS);
+
+    const double m = double(1u << K);
+    const double RSE = 1.04 / std::sqrt(m);
+    WARN("RSE = " << RSE);
 
     REQUIRE(estimate >= NUMBER_OF_UNIQUE_ELEMENTS * (1.0 - 3 * RSE));
     REQUIRE(estimate <= NUMBER_OF_UNIQUE_ELEMENTS * (1.0 + 3 * RSE));
